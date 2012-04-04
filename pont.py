cf2b2e8d24008d3a9ad9af56837f5a3eb0daba27
ktf <not.discostoo@gmail.com>
2012-04-04 13:24:53 -0700

    insert_spaces and split_into_fives functions complete

--- pont.py
+++ pont.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python
 
 
+import re
+
+
+def insert_spaces(instring, every=5):
+    """Insert spaces every "every" characters, 5 by default"""
+    outstring = ""
+    for i in xrange(0, len(instring), every):
+        outstring = outstring + instring[i:i+every] + " "
+    return outstring.strip() # gotta strip the last extraneous space
+
+
 def split_into_fives(instring):
-    """Split given string into groups of five characters"""
-    return instring
+    """Split given string into groups of five characters, separated by spaces
+    and capitalized"""
+    outstring = re.sub(r'\s', '', instring) # strip all whitespace
+
+    # add Xs so len is multiple of 5
+    if len(outstring) % 5 != 0:
+        num_x = 5 - (len(outstring) % 5)
+        outstring = outstring + ("X" * num_x)
+
+    # add spaces
+    outstring = insert_spaces(outstring)
+
+    return outstring.upper() # upper case
 
 
 def main():
--- test_pont.py
+++ test_pont.py
@@ -3,17 +3,36 @@
 
 import unittest
 
+from pont import insert_spaces
 from pont import split_into_fives
 
 
-class TestSplitIntoFives(unittest.TestCase):
-    def setUp(self):
-        pass
+class TestInsertSpaces(unittest.TestCase):
+
+    # in the form (instring, outstring)
+    test_strings = [("abcdefghijklm", "abcde fghij klm"),
+                    ("hey there man", "hey t here  man"),
+                    ("hey", "hey"),
+                    ("abcdeabcdeabcde", "abcde abcde abcde")]
 
     def test_basic_shit(self):
-        instring = "hey there"
-        outstring = "HEYTH EREXX"
-        self.assertEqual(instring, outstring)
+        """a bunch of basic strings should get transformed correctly!"""
+        for instring, outstring in self.test_strings:
+            self.assertEqual(insert_spaces(instring), outstring)
+
+
+class TestSplitIntoFives(unittest.TestCase):
+
+    # in the form (instring, outstring)
+    test_strings = [("hey there", "HEYTH EREXX"),
+                    ("hey", "HEYXX"),
+                    ("do not use PC", "DONOT USEPC"),
+                    ("welcome home", "WELCO MEHOM EXXXX")]
+
+    def test_basic_shit(self):
+        """split into groups of five and capitalize, man"""
+        for instring, outstring in self.test_strings:
+            self.assertEqual(split_into_fives(instring), outstring)
 
 
 if __name__ == "__main__":
